a5d5b93b90a351064bcc17d48f56cd4d7daeee50
mirella <m.pucciarini@agricolus.com>
2020-12-09 19:55:40 +0100

    color, font, basic style

--- src/modules/machines/main.vue
+++ src/modules/machines/main.vue
@@ -1,22 +1,35 @@
 <template>
-  <div>
-    <div v-for="machine in machines" :key="machine.id">
-      <ul>
-        <li>
-          <button @click="centerOnMap(machine)">
-            <i class="fas fa-crosshairs"></i>
-          </button>
-        </li>
-        <li v-for="(v, k) in machine" :key="k">
-          <span>{{ k }} : </span>
-          <span>{{ v }}</span>
-        </li>
-      </ul>
+  <div class="container">
+    <header>
+      <label> Machines </label>
+    </header>
+    <section class="scrollable">
+    <div v-for="machine in machines" :key="machine.id" class="card">
+      <div class="card-icon">
+        <img src="../../assets/img/tractor.svg">
+      </div>
+      <div class="card-body">
+        <div class="card-info">
+          {{machine.name}} - {{machine.code}}
+        </div>
+        <div class="card-info">
+          {{machine.description}}
+        </div>
+        <div class="card-info">
+          {{machine.producerCommercialName}}
+        </div>
+      </div>
+
+      <button @click="centerOnMap(machine)" class="btn btn-primary">
+        <i class="fas fa-crosshairs"></i>
+      </button>
+
     </div>
-    <div class="bottom">
-      <button>ADD MACHINE</button>
-      <button @click="startImport">IMPORT MACHINE FROM PRODUCER</button>
-    </div>
+    </section>
+    <footer>
+      <button class="btn btn-primary">ADD MACHINE</button>
+      <button class="btn btn-primary" @click="startImport">IMPORT MACHINE FROM PRODUCER</button>
+    </footer>
   </div>
 </template>
 <script lang="ts" src="./main.ts"></script>
